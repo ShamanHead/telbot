478f42239d1c91dd3bd110df9ec21f82176fc9f3
Романовский Арсений <ronyplay247@gmail.com>
2019-12-22 13:10:16 +0300

    v0.23r3b2

--- Example.php
+++ Example.php
@@ -1,37 +1,35 @@
 <?php
 
-/*
-	You can also check how work example at http://t.me/Testing1337002_bot
-*/
+require_once('lib/Bot.php');
+require_once('lib/Query.php');
+require_once('lib/Utils/Keyboard.php');
+require_once('lib/Utils/Context.php');
 
-require_once('lib/src/Bot.php');
-require_once('lib/src/Query.php');
-require_once('lib/3rd_party/Options.php');
-
-use \Telegram\Addons\Option as Option;
-use \Telegram\Query as Query;
-use \Telegram\Bot as Bot;
+use \Telbot\Utils\Context as Context;
+use \Telbot\Utils\Keyboard as Keyboard;
+use \Telbot\Query as Query;
+use \Telbot\Bot as Bot;
 
 $data = json_decode(file_get_contents('php://input'));
-$bot = new Bot('SOME BOT API KEY HERE', $data->message->chat->id);
+$bot = new Bot('927752546:AAGAnR8H_Aly22V-fIJEVE8srmRTzd_piYs', $data->message->chat->id);
 
-if(!Option::value($bot, 'read', $data->message->chat->id)){
+if(!Context::read($bot, $data->message->chat->id)){
 	Query::send($bot
 			,'sendMessage',
 		[
 			'text' => 'Write smth'
 		]
 	);
-	Option::value($bot, 'write', $data->message->chat->id, 'smth');
+	Context::write($bot, $data->message->chat->id, 'smth');
 }else{
 	Query::send($bot
 		,'sendMessage',
 		[
 			'text' => 'Okay, you writed!',
-			'reply_markup' => ['keyboard' => $bot->createKeyboard('keyboard', [ [ ['Smth'], ['Smth2'] ], [ ['smth3'], ['smth4'] ] ])]
+			'reply_markup' => ['keyboard' => Keyboard::create('keyboard', [ [ ['Smth'], ['Smth2'] ], [ ['smth3'], ['smth4'] ] ])]
 		]
 	);
-	Option::value($bot, 'delete', $data->message->chat->id, 'smth');
+	Context::delete($bot, $data->message->chat->id);
 }
 
 ?>
